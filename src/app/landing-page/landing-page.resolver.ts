33b70ed0a827187814616eaa95f764f9e624097f
Kevin Schneider <kschneider@ocas.ca>
2024-11-29 06:44:36 -0500

    Remove custom fetch to simplify example

--- src/app/landing-page/landing-page.resolver.ts
+++ src/app/landing-page/landing-page.resolver.ts
@@ -1,15 +1,11 @@
-import { HttpClient } from '@angular/common/http';
 import { inject, LOCALE_ID } from '@angular/core';
 import type { ActivatedRouteSnapshot, ResolveFn } from '@angular/router';
 import { fetchOneEntry, getBuilderSearchParams } from '@builder.io/sdk-angular';
-import { firstValueFrom } from 'rxjs';
 
 export const catchAllResolver: ResolveFn<any> = (
   route: ActivatedRouteSnapshot
 ) => {
-  const http = inject(HttpClient);
   const locale = inject(LOCALE_ID);
-
   const urlPath = `/${route.url.join('/')}`;
   const searchParams = getBuilderSearchParams(route.queryParams);
 
@@ -21,19 +17,5 @@ export const catchAllResolver: ResolveFn<any> = (
       urlPath,
     },
     options: searchParams,
-    fetch: async (url, init: any) => {  // todo: change into utility function... `httpClientFetch(http)`
-      const headers = init?.headers;
-      const resp = await firstValueFrom(http.get(url, { observe: 'response', responseType: 'text', headers }));
-
-      const headersInit: HeadersInit = {};
-      resp.headers.keys().forEach((key) => {
-        const val = resp.headers.get(key);
-        if (val) {
-          headersInit[key] = val;
-        }
-      });
-
-      return new Response(resp.body, { status: resp.status, statusText: resp.statusText, headers: headersInit });
-    }
   });
 };
